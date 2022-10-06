93c5e8427a7bb2143bde6521d0dc5e54b8c37265
Danush Ramanan <dan.ramanan@gmail.com>
2022-10-06 07:15:29 -0600

    check answers, but correct

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
 const form = $("form");
-const rightAnswers = ["emperor", "shogun", "daimyo","samurai", "ronin", "peasants", "artisans", "merchants", "outcasts", "3", "justice", "compassion", "honour", "loyalty", "courage", "honesty", "self-control"];
-let message = "You got them ALL right!! Great Job! :D";
+const reload = $("#reload");
+const rightAnswers = ["Emperor", "Shogun", "Daimyo","Samurai", "Ronin", "Peasants", "Artisans", "Merchants", "Outcasts", "3", "justice", "compassion", "honour", "loyalty", "courage", "honesty", "self-control"];
+let message = "Oh no! You messed up on";
+let isWrong, questionOneWrong, questionTwoWrong, questionThreeWrong = false;
 
 function checkAns() {
 	let answers = form.serializeArray();
-	console.log(answers);
 
-	if (answers.length !== rightAnswers.length) {
-		message = "Oh no! You messed up on question 3... :(";
-		return;
+	if (answers.length !== rightAnswers.length && !questionThreeWrong) {
+		message += "question 3\n";
+		questionThreeWrong = true;
 	}
 
 	for (let i = 0; i < answers.length; i++) {
-		if (answers[i] != rightAnswers[i]) {
-			if (i < 9) {
-				message = "Oh no! You messed up on question 1... :(";
-			}
-			else if (i > 9) {
-				message = "Oh no! You messed up on question 3... :(";
-			}
-			else {
-				message = "Oh no! You messed up on question 2... :(";
+		if (answers[i].value !== rightAnswers[i]) {
+			if (i < 9 && !questionOneWrong) {
+				message += "question 1\n";
+				questionOneWrong = true;
+			} else if (i === 9 && !questionTwoWrong) {
+				message += "question 2\n";				
+				questionTwoWrong = true;
+			} else if (i > 9 && !questionThreeWrong) {
+				message += "question 3\n";
+				questionThreeWrong = true;
 			}
 		}
+	}
+
+	if (questionOneWrong || questionTwoWrong || questionThreeWrong) {
+		isWrong = true;
+	}
+	if (!isWrong) {
+		message = "You got it right!!";
 	}
 }
 
@@ -32,26 +41,6 @@ form.on("submit", function (e) {
 	console.log(message);
 });
 
-/*
-10
-: 
-{name: 'virtues', value: 'Justice'}
-11
-: 
-{name: 'virtues', value: 'compassion'}
-12
-: 
-{name: 'virtues', value: 'honour'}
-13
-: 
-{name: 'virtues', value: 'loyalty'}
-14
-: 
-{name: 'virtues', value: 'courage'}
-15
-: 
-{name: 'virtues', value: 'honesty'}
-16
-: 
-{name: 'virtues', value: 'self-control'}
-*/
+reload.on("click", function() {
+	location.reload();
+});
